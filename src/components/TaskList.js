cd255559dab65b2d06fffd65bcf823b344e7c563
Aleksander Kazmierczak <aleksander.kazmierczak@gmail.com>
2020-09-24 22:47:14 +0200

    sort method

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -7,6 +7,9 @@ const TaskList = (props) => {
    const active = props.tasks.filter(task => task.active)
    const done = props.tasks.filter(task => !task.active)
 
+   done.sort((a, b) => b.finishDate - a.finishDate)
+   active.sort((a, b) => b.important - a.important)
+
    const activeTasks = active.map(task => <Task key={task.id} task={task} delete={props.delete} change={props.change} />)
    const doneTasks = done.map(task => <Task key={task.id} task={task} delete={props.delete} change={props.change} />)
 
