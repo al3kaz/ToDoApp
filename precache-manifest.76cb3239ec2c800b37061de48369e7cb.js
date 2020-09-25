1f234465af6da470c15a107b8461ff66ec7f6c92
Aleksander Kazmierczak <aleksander.kazmierczak@gmail.com>
2020-09-25 21:29:09 +0200

    Updates

---
+++ precache-manifest.76cb3239ec2c800b37061de48369e7cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e06d1d0ddb19c158b3eee69c9ee9da7",
+    "url": "/ToDoApp/index.html"
+  },
+  {
+    "revision": "7861cb123659379d4ee2",
+    "url": "/ToDoApp/static/css/main.84a1676a.chunk.css"
+  },
+  {
+    "revision": "9070af7ac58b8e6c266c",
+    "url": "/ToDoApp/static/js/2.ca07993d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDoApp/static/js/2.ca07993d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7861cb123659379d4ee2",
+    "url": "/ToDoApp/static/js/main.79eee7e8.chunk.js"
+  },
+  {
+    "revision": "ccd1afadff0d4c59772c",
+    "url": "/ToDoApp/static/js/runtime-main.ec428e81.js"
+  }
+]);
